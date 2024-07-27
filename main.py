fd2ff5a2c1e9fc7e3360abe6711d858ca5cde414
Pham Anh Tien <anhtien13082005@gmail.com>
2024-07-27 22:30:08 +0700

    update data validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Sleep_Efficiency_App import logger
 from Sleep_Efficiency_App.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Sleep_Efficiency_App.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -9,6 +10,19 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
