df259a1b9c3f747a6616fd3aa413f03b4696fac9
Pham Anh Tien <anhtien13082005@gmail.com>
2024-07-28 00:07:33 +0700

    update data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Sleep_Efficiency_App import logger
 from Sleep_Efficiency_App.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Sleep_Efficiency_App.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from Sleep_Efficiency_App.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -22,6 +23,19 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
     logger.exception(e)
